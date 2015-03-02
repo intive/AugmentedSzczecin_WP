00b4fe0217be62751234de69c78c8abc3a6191ed
Borys <borys.rybak@gmail.com>
2015-03-02 16:22:36 +0100

    Added DataService service

--- AugmentedSzczecin/AugmentedSzczecin/ViewModels/LocationListViewModel.cs
+++ AugmentedSzczecin/AugmentedSzczecin/ViewModels/LocationListViewModel.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using AugmentedSzczecin.Models;
+using AugmentedSzczecin.Services;
+using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Data.Json;
 
 namespace AugmentedSzczecin.ViewModels
 {
-	public class LocationListViewModel
+	public class LocationListViewModel : PropertyChangedBase
 	{
+		private DataService Client = new DataService();
+
+		public LocationListViewModel()
+		{
+			LoadData();
+		}
+
+		private async void LoadData()
+		{
+			Places = await Client.RunAsync();
+		}
+
+		List<Place> _places = new List<Place>();
+		public List<Place> Places
+		{
+			get {  return _places; }
+			set
+			{
+				if(_places != value)
+				{
+					_places = value;
+					NotifyOfPropertyChange(() => Places);
+				}
+			}
+		}
 	}
 }
