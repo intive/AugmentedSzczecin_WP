0e0e1214dd497545d96961281abfd2e803c44137
Borys <borys.rybak@gmail.com>
2015-03-02 19:43:43 +0100

    Added working solution

--- AugmentedSzczecin/AugmentedSzczecin/Services/DataService.cs
+++ AugmentedSzczecin/AugmentedSzczecin/Services/DataService.cs
@@ -27,11 +27,12 @@ namespace AugmentedSzczecin.Services
 			response.EnsureSuccessStatusCode();
 			string jsonString = await response.Content.ReadAsStringAsync();
 
+			//ALTERNATIVE SOLUTION
 			DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(ObservableCollection<Place>));
 			MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
 			model = (ObservableCollection<Place>)ser.ReadObject(stream);
 
-			//IMPORTANT!!!
+			//ORIGINAL SOLUTION - nulls
 			//model = JsonConvert.DeserializeObject<ObservableCollection<Place>>(jsonString);
 			//string x = "[{ \"id\": \"112C8C46-C35B-4A41-A213-C627CFF9351F\", \"Name\": \"WI ZUT\", \"Address\": \"ul. Żołnierska 49\", \"Latitude\": 47.6785619, \"Longitude\": -122.1311156, \"HasWifi\": true }, { \"id\": \"112C8C46-C35B-4A41-A213-C627CFF9351F\", \"Name\": \"WI ZUT\", \"Address\": \"ul. Żołnierska 49\", \"Latitude\": 47.6785619, \"Longitude\": -122.1311156, \"HasWifi\": true }]";
 			//model = JsonConvert.DeserializeObject<ObservableCollection<Place>>(x);
