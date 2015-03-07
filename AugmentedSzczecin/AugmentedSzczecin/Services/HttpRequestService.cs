a29d6f9cd1cdc44e9306b4fe7f7f8fc70872f002
Borys <borys.rybak@gmail.com>
2015-03-07 23:39:31 +0100

    Added PointOfInterest class model

--- AugmentedSzczecin/AugmentedSzczecin/Services/HttpRequestService.cs
+++ AugmentedSzczecin/AugmentedSzczecin/Services/HttpRequestService.cs
@@ -15,7 +15,6 @@ namespace AugmentedSzczecin.Services
 		private static string _page = "https://patronatwp.azure-mobile.net/tables/Place";
 		public async Task<string> HttpGetAsync()
 		{
-			ObservableCollection<Place> model = new ObservableCollection<Place>();
 			HttpClient client = new HttpClient();
 
 			HttpResponseMessage response = await client.GetAsync(_page);
