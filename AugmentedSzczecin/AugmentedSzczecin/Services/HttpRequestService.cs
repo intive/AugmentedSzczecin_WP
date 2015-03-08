0c95b9e6cf21723c7cdc828d9068e8c57c02606b
Borys <borys.rybak@gmail.com>
2015-03-08 19:27:23 +0100

    Revert "Added sample view of POI on ListView"
    This reverts commit d22b9b1995ecdad0d7f50903f545a7ae0e74f5ac.

--- AugmentedSzczecin/AugmentedSzczecin/Services/HttpRequestService.cs
+++ AugmentedSzczecin/AugmentedSzczecin/Services/HttpRequestService.cs
@@ -12,7 +12,7 @@ namespace AugmentedSzczecin.Services
 {
 	public class HttpRequestService : IHttpRequestService
 	{
-		private static string _page = "https://augmented-szczecin-test.azure-mobile.net/tables/PointOfInterest";
+		private static string _page = "https://patronatwp.azure-mobile.net/tables/Place";
 		public async Task<string> HttpGetAsync()
 		{
 			HttpClient client = new HttpClient();
