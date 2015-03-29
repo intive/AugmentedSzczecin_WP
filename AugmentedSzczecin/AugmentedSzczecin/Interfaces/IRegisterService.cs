cd89483a199e24d7850b091738eca4dd61ddea7f
bobista0 <borys.rybak@gmail.com>
2015-03-29 16:07:36 +0200

    Added IRegisterService

---
+++ AugmentedSzczecin/AugmentedSzczecin/Interfaces/IRegisterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AugmentedSzczecin.Interfaces
+{
+    public interface IRegisterService
+    {
+        void Register(string email, string password);
+    }
+}
