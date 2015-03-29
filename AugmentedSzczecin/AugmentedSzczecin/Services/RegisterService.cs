e76b67c2c5f4a33e2a3fa239bc6386bef94fdb3b
bobista0 <borys.rybak@gmail.com>
2015-03-29 16:09:19 +0200

    Added empty RegisterService

---
+++ AugmentedSzczecin/AugmentedSzczecin/Services/RegisterService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AugmentedSzczecin.Interfaces;
+
+namespace AugmentedSzczecin.Services
+{
+    public class RegisterService : IRegisterService
+    {
+
+        public async void Register(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
