d160fdbfe12b16675dafd7143787ad5cceaf9fae
Agata <aborninska@wi.zut.edu.pl>
2015-03-29 19:35:56 +0200

    Added messages to string resources.

--- AugmentedSzczecin/AugmentedSzczecin/ViewModels/SignUpViewModel.cs
+++ AugmentedSzczecin/AugmentedSzczecin/ViewModels/SignUpViewModel.cs
@@ -189,18 +189,19 @@ namespace AugmentedSzczecin.ViewModels
 
         private async void WrongValidationMessageDialog()
         {
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
             string Message = "";
             if (!IsEmailEmptyValid)
-                Message += "Nie podano adresu e-mail.\n";
+                Message += loader.GetString("SignUpEmailEmpty") + "\n";
 
             if (!IsEmailMatchValid && IsEmailEmptyValid)
-                Message += "Podano błędny adres e-mail.\n";
+                Message += loader.GetString("SignUpEmailMatch") + "\n";
 
             if (!IsPasswordLengthValid && IsPasswordEmptyValid)
-                Message += "Hasło jest za krótkie.\n";
+                Message += loader.GetString("SignUpPasswordLength") + "\n";
 
             if (!IsPasswordEmptyValid)
-                Message += "Nie podano hasła.\n";
+                Message += loader.GetString("SignUpPasswordEmpty");
 
             var Msg = new MessageDialog(Message);
             await Msg.ShowAsync();
