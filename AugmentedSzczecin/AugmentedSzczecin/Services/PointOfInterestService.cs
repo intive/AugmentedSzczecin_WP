767d088ad217cce2463e2e8bd6089e81acd7e25f
bobista0 <borys.rybak@gmail.com>
2015-04-01 20:36:46 +0200

    Fixed PointOfInterestService.cs - Ordered code

--- AugmentedSzczecin/AugmentedSzczecin/Services/PointOfInterestService.cs
+++ AugmentedSzczecin/AugmentedSzczecin/Services/PointOfInterestService.cs
@@ -12,6 +12,8 @@ namespace AugmentedSzczecin.Services
 {
     public class PointOfInterestService : IPointOfInterestService
     {
+        private const string TemporaryPointOfInterestDatabaseUri = "https://augmented-szczecin-test.azure-mobile.net/tables/PointOfInterest";
+
         private readonly IEventAggregator _eventAggregator;
         public PointOfInterestService(IEventAggregator eventAggregator)
         {
@@ -24,13 +26,11 @@ namespace AugmentedSzczecin.Services
             return model;
         }
 
-        private readonly string _page = "https://augmented-szczecin-test.azure-mobile.net/tables/PointOfInterest";
-
         public async Task<string> GetPointOfInterestsJsonString()
         {
             HttpClient client = new HttpClient();
 
-            HttpResponseMessage response = await client.GetAsync(_page);
+            HttpResponseMessage response = await client.GetAsync(TemporaryPointOfInterestDatabaseUri);
             response.EnsureSuccessStatusCode();
             string jsonString = await response.Content.ReadAsStringAsync();
 
