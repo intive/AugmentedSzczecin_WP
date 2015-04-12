31a443b4f0df70e4506317276926163c72c2b5e0
kwozniak <kacper_wozniak@outlook.com>
2015-04-12 19:37:30 +0200

    Added POI To Geopoint converter

---
+++ AugmentedSzczecin/AugmentedSzczecin/Converters/PoiToGeopointConverter.cs
@@ -0,0 +1,22 @@
+﻿using AugmentedSzczecin.Models;
+using System;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace AugmentedSzczecin.Converters
+{
+    public class PoiToGeopointConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            PointOfInterest poi = (PointOfInterest)value;
+            return new Geopoint(new BasicGeoposition() { Latitude = poi.Latitude, Longitude = poi.Longitude });
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
