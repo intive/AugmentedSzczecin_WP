87138da39ca80e3c715b1ca135ab8fd6e0454866
Agata <aborninska@wi.zut.edu.pl>
2015-04-16 20:15:20 +0200

    Added ResetPassword view and viewmodel

--- AugmentedSzczecin/AugmentedSzczecin/App.xaml.cs
+++ AugmentedSzczecin/AugmentedSzczecin/App.xaml.cs
@@ -31,6 +31,7 @@ namespace AugmentedSzczecin
             _container.PerRequest<LocationListViewModel>();
             _container.PerRequest<SignUpViewModel>();
             _container.PerRequest<SignInViewModel>();
+            _container.PerRequest<ResetPasswordViewModel>();
             _container.PerRequest<IPointOfInterestService, PointOfInterestService>();
             _container.PerRequest<ILocationService, LocationService>();
             _container.PerRequest<IAccountService, AccountService>();
