3cabd0024ab14d5084bb057a87d83493ea0ce42f
Agata <aborninska@wi.zut.edu.pl>
2015-04-16 20:26:07 +0200

    Added rest of resetpassword view and viewmodel

--- AugmentedSzczecin/AugmentedSzczecin/ViewModels/ResetPasswordViewModel.cs
+++ AugmentedSzczecin/AugmentedSzczecin/ViewModels/ResetPasswordViewModel.cs
@@ -1,12 +1,203 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AugmentedSzczecin.Events;
+using AugmentedSzczecin.Interfaces;
+using Caliburn.Micro;
+using System;
+using System.Text.RegularExpressions;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Popups;
 
 namespace AugmentedSzczecin.ViewModels
 {
-    class ResetPasswordViewModel
+    public class ResetPasswordViewModel : Screen
     {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IAccountService _accountService;
+
+        public ResetPasswordViewModel(IEventAggregator eventAggregator, IAccountService accountService)
+        {
+            _eventAggregator = eventAggregator;
+            _accountService = accountService;
+        }
+
+        private bool _isProgressRingVisible;
+        public bool IsProgressRingVisible
+        {
+            get
+            {
+                return _isProgressRingVisible;
+            }
+            set
+            {
+                if (value != _isProgressRingVisible)
+                {
+                    _isProgressRingVisible = value;
+                    NotifyOfPropertyChange(() =>IsProgressRingVisible);
+                }
+            }
+        }
+
+        private bool _isProgressRingActive;
+        public bool IsProgressRingActive
+        {
+            get
+            {
+                return _isProgressRingActive;
+            }
+            set
+            {
+                if (value != _isProgressRingActive)
+                {
+                    _isProgressRingActive = value;
+                    NotifyOfPropertyChange(() => IsProgressRingActive);
+                }
+            }
+        }
+
+        private bool _areControlsEnabled = true;
+        public bool AreControlsEnabled
+        {
+            get
+            {
+                return _areControlsEnabled;
+            }
+            set
+            {
+                if (value != _areControlsEnabled)
+                {
+                    _areControlsEnabled = value;
+                    NotifyOfPropertyChange(() => AreControlsEnabled);
+                }
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    ValidateEmailEmpty();
+                    ValidateEmailMatch();
+                    NotifyOfPropertyChange(() => Email);
+                }
+            }
+        }
+
+        private bool _isEmailEmptyValid;
+        public bool IsEmailEmptyValid
+        {
+            get
+            {
+                return _isEmailEmptyValid;
+            }
+            set
+            {
+                if (_isEmailEmptyValid != value)
+                {
+                    _isEmailEmptyValid = value;
+                    NotifyOfPropertyChange(() => IsEmailEmptyValid);
+                }
+            }
+        }
+
+        private bool _isEmailMatchValid;
+        public bool IsEmailMatchValid
+        {
+            get
+            {
+                return _isEmailMatchValid;
+            }
+            set
+            {
+                if (_isEmailMatchValid != value)
+                {
+                    _isEmailMatchValid = value;
+                    NotifyOfPropertyChange(() => IsEmailMatchValid);
+                }
+            }
+        }
+
+        private bool _validationCheck;
+        public bool ValidationCheck
+        {
+            get
+            {
+                return _validationCheck;
+            }
+            set
+            {
+                _validationCheck = value;
+                NotifyOfPropertyChange(() => ValidationCheck);
+            }
+        }
+
+        protected override void OnActivate()
+        {
+            _eventAggregator.Subscribe(this);
+            base.OnActivate();
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+            _eventAggregator.Unsubscribe(this);
+            base.OnDeactivate(close);
+        }
+
+        public void Reset()
+        {
+            CheckValidation();
+            if (ValidationCheck)
+            {
+                AreControlsEnabled = false;
+                IsProgressRingVisible = true;
+                IsProgressRingActive = true;
+            }
+            else
+            {
+                WrongValidationMessageDialog();
+            }
+
+        }
+
+        private void ValidateEmailEmpty()
+        {
+            IsEmailEmptyValid = !String.IsNullOrEmpty(Email);
+        }
+
+        private void ValidateEmailMatch()
+        {
+            IsEmailMatchValid = Regex.IsMatch(Email,
+                @"^(?("")(""[^""]+?""@)|(([0-9a-zA-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-z][-\w]*[0-9a-zA-z]*\.)+[a-zA-z0-9]{2,24}))$");
+        }
+
+        private void CheckValidation()
+        {
+            ValidationCheck = IsEmailEmptyValid && IsEmailMatchValid;
+        }
+
+        private async void WrongValidationMessageDialog()
+        {
+            var loader = new ResourceLoader();
+            string message = "";
+
+            if (!IsEmailEmptyValid)
+            {
+                message += loader.GetString("SignUpEmailEmpty") + "\n";
+            }
+            if (!IsEmailMatchValid && IsEmailEmptyValid)
+            {
+                message += loader.GetString("SignUpEmailMatch") + "\n";
+            }
+
+            var msg = new MessageDialog(message);
+            await msg.ShowAsync();
+        }
     }
 }
