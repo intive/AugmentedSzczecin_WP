06558eb074ced97db3f34ee59490de522b904687
Agata <aborninska@wi.zut.edu.pl>
2015-05-19 23:41:48 +0200

    Added SideMenuFilter user control - xaml and interface

---
+++ AugmentedSzczecin/AugmentedSzczecin/Interfaces/ISideMenuFilter.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+
+namespace AugmentedSzczecin.Interfaces
+{
+    public class ISideMenuFilter : Screen
+    {
+        private string _selectedCategory;
+        public string SelectedCategory
+        {
+            get
+            {
+                return _selectedCategory;
+            }
+            set
+            {
+                if (value != _selectedCategory)
+                {
+                    _selectedCategory = value;
+                    NotifyOfPropertyChange(() => SelectedCategory);
+                }
+            }
+        }
+
+    }
+}
