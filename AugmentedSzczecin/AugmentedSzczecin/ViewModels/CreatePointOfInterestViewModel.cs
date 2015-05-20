30de23f23568ea4cf955ec1259d71fd0ac289e6f
bobista0 <borys.rybak@gmail.com>
2015-05-20 17:12:21 +0200

    Added creating POI fields

---
+++ AugmentedSzczecin/AugmentedSzczecin/ViewModels/CreatePointOfInterestViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Windows.Devices.Geolocation;
+using Caliburn.Micro;
+
+namespace AugmentedSzczecin.ViewModels
+{
+    public class CreatePointOfInterestViewModel : Screen
+    {
+        public Geopoint Parameter { get; set; }
+
+        private string _latitude;
+        public string Latitude
+        {
+            get { return _latitude; }
+            set
+            {
+                if (_latitude != value)
+                {
+                    _latitude = value;
+                    NotifyOfPropertyChange(() => Latitude);
+                }
+            }
+        }
+
+        private string _longitude;
+        public string Longitude
+        {
+            get { return _longitude; }
+            set
+            {
+                if (_longitude != value)
+                {
+                    _longitude = value;
+                    NotifyOfPropertyChange(() => Longitude);
+                }
+            }
+        }
+
+        protected override void OnActivate()
+        {
+            Latitude = Parameter.Position.Latitude.ToString();
+            Longitude = Parameter.Position.Longitude.ToString();
+        }
+
+    }
+}
