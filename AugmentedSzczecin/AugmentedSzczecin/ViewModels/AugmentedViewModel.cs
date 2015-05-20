09ab33fb2aaea04f5a1a2f4f0c5c7ffe695e7a5f
kwozniak <kacper_wozniak@outlook.com>
2015-05-20 23:45:41 +0200

    Added radius property.

--- AugmentedSzczecin/AugmentedSzczecin/ViewModels/AugmentedViewModel.cs
+++ AugmentedSzczecin/AugmentedSzczecin/ViewModels/AugmentedViewModel.cs
@@ -1,22 +1,40 @@
 ﻿using AugmentedSzczecin.Interfaces;
 using Caliburn.Micro;
+using Windows.Devices.Geolocation;
 namespace AugmentedSzczecin.ViewModels
 {
     public class AugmentedViewModel : Screen
     {
         private readonly IEventAggregator _eventAggregator;
         private readonly IPointOfInterestService _pointOfInterestService;
+        private readonly ILocationService _locationService;
+
+        private int _radius = 200;
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+                NotifyOfPropertyChange(() => Radius);
+            }
+        }
 
         public AugmentedViewModel(IEventAggregator eventAggregator, ILocationService locationService, IPointOfInterestService pointOfInterestService, INavigationService navigationService)
         {
             _eventAggregator = eventAggregator;
             _pointOfInterestService = pointOfInterestService;
+            _locationService = locationService;
         }
-        protected override void OnActivate()
+        protected override async void OnActivate()
         {
             base.OnActivate();
             _eventAggregator.Subscribe(this);
-            _pointOfInterestService.Refresh();
+            Geopoint point = await _locationService.GetGeolocation();
+            _pointOfInterestService.LoadPoIs(point.Position.Latitude, point.Position.Longitude, Radius);
         }
 
         protected override void OnDeactivate(bool close)
