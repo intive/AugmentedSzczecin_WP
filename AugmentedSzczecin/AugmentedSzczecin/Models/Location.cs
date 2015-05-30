7bd425f8237590fa5c5237374b1ddb1c927c5e79
bobista0 <borys.rybak@gmail.com>
2015-05-30 16:51:31 +0200

    Separated classes from Models/PointOfInterest.cs

---
+++ AugmentedSzczecin/AugmentedSzczecin/Models/Location.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using Newtonsoft.Json;
+
+namespace AugmentedSzczecin.Models
+{
+
+    public class Location : PropertyChangedBase
+    {
+        [JsonProperty(PropertyName = "latitude")]
+        private double _latitude;
+        public double Latitude
+        {
+            get
+            {
+                return _latitude;
+            }
+            set
+            {
+                if (_latitude != value)
+                {
+                    _latitude = value;
+                    NotifyOfPropertyChange(() => Latitude);
+                }
+            }
+        }
+
+        [JsonProperty(PropertyName = "longitude")]
+        private double _longitude;
+        public double Longitude
+        {
+            get
+            {
+                return _longitude;
+            }
+            set
+            {
+                if (_longitude != value)
+                {
+                    _longitude = value;
+                    NotifyOfPropertyChange(() => Longitude);
+                }
+            }
+        }
+    }
+}
