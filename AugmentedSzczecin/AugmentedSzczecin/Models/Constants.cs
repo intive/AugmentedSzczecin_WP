4174b35a17809b5b569f81c93eedc3da2fb3c743
bobista0 <borys.rybak@gmail.com>
2015-06-02 19:48:54 +0200

    Added Constants static class

---
+++ AugmentedSzczecin/AugmentedSzczecin/Models/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace AugmentedSzczecin.Models
+{
+    public static class Constants
+    {
+        public static string BingKey
+        {
+            get { return "AsaWb7fdBJmcC1YW6uC1UPb57wfLh9cmeX6Zq_r9s0k49tFScWa3o3Z0Sk7ZUo3I"; }
+        }
+    }
+}
