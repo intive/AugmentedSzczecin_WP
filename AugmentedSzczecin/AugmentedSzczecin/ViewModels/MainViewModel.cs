2f68d28a2e8ce75663d948e3e26fc03fe7f553dd
bobista0 <borys.rybak@gmail.com>
2015-06-05 01:11:47 +0200

    Added automated animation to FlipView

--- AugmentedSzczecin/AugmentedSzczecin/ViewModels/MainViewModel.cs
+++ AugmentedSzczecin/AugmentedSzczecin/ViewModels/MainViewModel.cs
@@ -1,7 +1,15 @@
-﻿using AugmentedSzczecin.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AugmentedSzczecin.Interfaces;
 using Caliburn.Micro;
 using Windows.ApplicationModel.Resources;
 using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using AugmentedSzczecin.Views;
 
 namespace AugmentedSzczecin.ViewModels
 {
@@ -9,11 +17,112 @@ namespace AugmentedSzczecin.ViewModels
     {
         private readonly INavigationService _navigationService;
         private readonly IAccountService _accountService;
+        private readonly DispatcherTimer _timer;
 
         public MainViewModel(INavigationService navigationService, IAccountService accountService)
         {
             _navigationService = navigationService;
             _accountService = accountService;
+
+            _timer = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromSeconds(2)
+            };
+
+            _timer.Tick += ChangeImage;
+            _timer.Start();
+        }
+
+        private int _selectedBackground;
+        public int SelectedBackground
+        {
+            get { return _selectedBackground; }
+            set
+            {
+                if (_selectedBackground != value)
+                {
+                    _selectedBackground = value;
+                    NotifyOfPropertyChange(() => SelectedBackground);
+                }
+            }
+        }
+
+        private List<FlipViewItem> _backgroundList = GetBackgroundList();
+        public List<FlipViewItem> BackgroundList
+        {
+            get { return _backgroundList; }
+            set
+            {
+                if (_backgroundList != value)
+                {
+                    _backgroundList = value;
+                    NotifyOfPropertyChange(() => BackgroundList);
+                }
+            }
+        }
+
+        private static List<FlipViewItem> GetBackgroundList()
+        {
+            return new List<FlipViewItem>()
+            {
+                new FlipViewItem()
+                {
+                    Background = new ImageBrush()
+                    {
+                        Stretch = Stretch.Fill,
+                        ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SampleBackgrounds/szczecin_01.png"))
+                    }
+                },
+                new FlipViewItem()
+                {
+                    Background = new ImageBrush()
+                    {
+                        Stretch = Stretch.Fill,
+                        ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SampleBackgrounds/szczecin_02.png"))
+                    }
+                },
+                new FlipViewItem()
+                {
+                    Background = new ImageBrush()
+                    {
+                        Stretch = Stretch.Fill,
+                        ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SampleBackgrounds/szczecin_03.png"))
+                    }
+                },
+                new FlipViewItem()
+                {
+                    Background = new ImageBrush()
+                    {
+                        Stretch = Stretch.Fill,
+                        ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SampleBackgrounds/szczecin_04.png"))
+                    }
+                },
+                new FlipViewItem()
+                {
+                    Background = new ImageBrush()
+                    {
+                        Stretch = Stretch.Fill,
+                        ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SampleBackgrounds/szczecin_05.png"))
+                    }
+                },
+                new FlipViewItem()
+                {
+                    Background = new ImageBrush()
+                    {
+                        Stretch = Stretch.Fill,
+                        ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SampleBackgrounds/szczecin_06.png"))
+                    }
+                }
+            };
+        }
+
+        private void ChangeImage(object sender, object o)
+        {
+            var totalItems = BackgroundList.Count;
+
+            var newItemIndex = (SelectedBackground + 1) % totalItems;
+
+            SelectedBackground = newItemIndex;
         }
 
         public void NavigateToAbout()
@@ -80,6 +189,11 @@ namespace AugmentedSzczecin.ViewModels
         public void NavigateToAugmentedView()
         {
             _navigationService.NavigateToViewModel<AugmentedViewModel>();
+        }
+
+        public void DisplayedItemChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
